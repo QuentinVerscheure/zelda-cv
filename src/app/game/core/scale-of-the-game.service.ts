20141a431d9d3b63f0079f8159300b4de62ac9cf
Verscheure Quentin <quentin.verscheure@gmail.com>
2024-08-23 17:54:34 +0200

    fix scale of the game for smartphone and 2k, 4k

--- src/app/game/core/scale-of-the-game.service.ts
+++ src/app/game/core/scale-of-the-game.service.ts
@@ -16,12 +16,16 @@ static calculatescaleOfTheGame(){
   let scaleOfTheGame:number;
 
   // Set scaleOfTheGame based on screen width
-  if (width >= 1024) {
+  if (width >= 3840) {
+    scaleOfTheGame = 8; // 4k
+  } else if (width >= 2560) {
+    scaleOfTheGame = 6; // 2k
+  } else if (width >= 1024) {
     scaleOfTheGame = 4; // Desktop
   } else if (width >= 768) {
     scaleOfTheGame = 3; // Tablets
   } else {
-    scaleOfTheGame = 2; // Smartphones
+    scaleOfTheGame = 3; // Smartphones
   }
 
   return scaleOfTheGame;
